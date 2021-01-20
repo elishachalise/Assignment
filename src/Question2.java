c34a13a75b10c9c891d0027bc1229937908d3613
Elisha Chalise <elishachalise05@gmail.com>
2021-01-20 15:51:16 -0600

    Question No:2

---
+++ src/Question2.java
@@ -0,0 +1,8 @@
+public class Question2 {
+    public static void main(String[] args) {
+        System.out.println("My Name is Elisha Chalise.");
+        System.out.println("My Birthplace is Nepal.");
+        System.out.println("I am a married women.");
+
+    }
+}
