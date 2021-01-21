c509ea3cff986ba85cddbdd93becb1a68e2caeb0
Elisha Chalise <elishachalise05@gmail.com>
2021-01-20 19:59:20 -0600

    Assignment 11a

---
+++ src/Assignment11a.java
@@ -0,0 +1,15 @@
+public class Assignment11a {
+
+    public static void main(String[] args) {
+        int a=10,b=20,temp;
+        System.out.println("Before swap Value of a: "+a);
+        System.out.println("Before swap Value of b: "+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swap Value of a: "+a);
+        System.out.println("After swap Value of b: "+b);
+
+
+    }
+}
