b6c7d03b82f382f3dc7e978e495c87796635ff4a
Elisha Chalise <elishachalise05@gmail.com>
2021-01-21 09:31:11 -0600

    Assignment 11b

---
+++ src/Assignment11b.java
@@ -0,0 +1,12 @@
+public class Assignment11b {
+    public static void main(String[] args) {
+        int a=10, b=20;
+          a = a - b;
+          b = a + b;
+        a = b - a;
+        System.out.println("value of a after swap: " +a);
+        System.out.println("value of a after swap: " +b);
+
+    }
+
+}
